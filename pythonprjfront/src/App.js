4f49a3fe2a371c0b2c02f2fc708917ced5b8679c
gsot8 <gsot2006@gmail.com>
2024-02-01 18:37:49 +0300

    Теперь и с фронтом

--- pythonprjfront/src/App.js
+++ pythonprjfront/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import CreateRooms from './CreateRoom';
 import TimetableCreate from './TimetableCreate';
+import RoomsCreate from './RoomsCreate';
+
 
 function App() {
-  return <div>
-    <TimetableCreate></TimetableCreate>
-    
-    </div>;
+  return <BrowserRouter>
+  <Link to='/CreateTimetable'>DDDD</Link>
+  <Link to='/CreateRooms'>FFFFF</Link>
+  <Routes>
+    <Route path="CreateTimetable" element={<TimetableCreate />} />
+    <Route path="CreateRooms" element={<RoomsCreate />} />
+  </Routes>
+</BrowserRouter>
 }
 
 export default App;
--- pythonprjfront/src/TimetableCreate.js
+++ pythonprjfront/src/TimetableCreate.js
@@ -1,11 +1,18 @@
 import GetAllTimeBook from './GetAllTimeBook';
 import {useState} from "react";
+import {useEffect} from "react";
 function TimetableCreate(){
     const [inputCount, setInputCount] = useState("")
     const [inputFloor, setInputFloor] = useState("")
     const [inputDate, setInputDate] = useState("")
+    const [idRoom,setIdRoom] = useState("")
+    const[rooms,setRooms] = useState([])
     const[flag,setFlag] = useState(false)
+    useEffect(() => {
+        fetch("http://localhost:5000/room").then((response) => response.json()
+          ).then((res) => {setRooms(res); setIdRoom(res[0].number)})
 
+     }, []);
     return(
         <div>
         <div>
@@ -15,11 +22,14 @@ function TimetableCreate(){
             type = "datetime-local"
   value={inputDate} onInput={e => setInputDate(e.target.value)}
    />
+            <select onSelect={e => setIdRoom(e.target.value)}>
+            {rooms.map((jso) => <option>{jso.number}</option>)}
+            </select>
             <button onClick={
                 () => {fetch("http://localhost:5000/rooms/book", 
                 {  
                       method: "POST",  
-                      body: JSON.stringify({count: inputCount,floor : inputFloor,created_date: inputDate}),  
+                      body: JSON.stringify({id_room: idRoom ,count: inputCount,floor : inputFloor,created_date: inputDate}),  
                       headers: {'Content-Type': 'application/json'},  // так как мы шлём json, нужно явно это показать
                   }
                ).then(r => setFlag(!flag)).catch(e=> console.log(e))
